1e03e9c364b145b5f088e00b9f4408b1c6ef4d8b
wilimartinez <90808126+wilimartinez@users.noreply.github.com>
2021-09-16 19:14:10 -0500

    intento

--- src/intento1/Intento1.java
+++ src/intento1/Intento1.java
@@ -17,6 +17,8 @@ public class Intento1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        
+        System.out.println("Esto es un intento");
     }
     
 }
